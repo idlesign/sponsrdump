f621c47b6bf48d636b7a3ec431e3e59b77fc49de
idle sign <idlesign@yandex.ru>
2023-09-11 18:25:25 +0700

    Добавлена поддержка новых опций.

--- sponsrdump.py
+++ sponsrdump.py
@@ -619,19 +619,31 @@ class SponsrDumper:
 
 
 if __name__ == '__main__':
-
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('project_url', help='URL проекта на sponsr.ru')
-    parser.add_argument('--debug', help='Вывести отладочную информацию', action='store_true')
-    parser.add_argument('--title', help='Фильтр заголовка для отбора статей', default='')
-    parser.add_argument('--to', help='Путь назначения для файлов', default='dump/')
-    parser.add_argument('--prefer-video', help='Предпочтительное разрешение видео', default='best')
+    parser.add_argument(
+        'project_url', help='URL проекта на sponsr.ru')
+    parser.add_argument(
+        '--debug', help='Вывести отладочную информацию', action='store_true')
+    parser.add_argument(
+        '--title', help='Фильтр заголовка для отбора статей', default='')
+    parser.add_argument(
+        '--to', help='Путь назначения для файлов', default='dump/')
+    parser.add_argument(
+        '--prefer-video', help='Предпочтительное разрешение видео', default='best')
+    parser.add_argument(
+        '--no-audio', help='Не следует скачивать аудио', action='store_true')
+    parser.add_argument(
+        '--no-video', help='Не следует скачивать видео', action='store_true')
+    parser.add_argument(
+        '--no-text', help='Не следует скачивать текст', action='store_true')
+    parser.add_argument(
+        '--text-to-video', help='Следует ли создать видео с текстом статьи', action='store_true')
 
     args = parser.parse_args()
 
-    logging.basicConfig(level=logging.DEBUG if args.debug else logging.INFO, format='%(levelname)s: %(message)s')
+    logging.basicConfig(level=logging.DEBUG if args.debug else logging.INFO, format='%(levelname)-8s: %(message)s')
 
     dumper = SponsrDumper(args.project_url)
 
@@ -641,4 +653,11 @@ if __name__ == '__main__':
         filter_func = lambda post_info: title in post_info['post_title']
 
     dumper.search(func_filter=filter_func)
-    dumper.dump(args.to, prefer_video=VideoPreference(frame=args.prefer_video))
+    dumper.dump(
+        args.to,
+        prefer_video=VideoPreference(frame=args.prefer_video),
+        audio=not args.no_audio,
+        video=not args.no_video,
+        text=not args.no_text,
+        text_to_video=args.text_to_video
+    )
