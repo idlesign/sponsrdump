452f02c02c88e1f153277d651068c36bc9fc54a7
idle sign <idlesign@yandex.ru>
2025-01-17 11:40:56 +0700

    Не пытаемся скачать аудио с неуказанной продолжительностью (see #3).

--- sponsrdump.py
+++ sponsrdump.py
@@ -396,7 +396,10 @@ class SponsrDumper:
         }
 
         for file_info in post.get('files') or []:
-            assert file_info['file_category'] == 'podcast', f"Unsupported file category found: {file_info}"
+            assert file_info['file_category'] == 'podcast', f'Unsupported file category found: {file_info}'
+            if not file_info['file_duration']:
+                LOGGER.debug(f'Probably missing {file_info['file_link']}. Skipped.')
+                continue
             audio.append(file_info)
 
         post_title = post['post_title'].rstrip('.')
